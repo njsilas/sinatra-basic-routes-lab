d77ac3c7211c3a48447fb0ff8fd10fd250f0f37a
Nicholas Silas <njsilas@gmail.com>
2020-11-30 22:48:08 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Nick"
+  end
+  get '/hometown' do
+    "My hometown is Detroit"
+  end
+  get '/favorite-song' do
+    "My favorite song is ha"
+  end
+
 end
